8aa855fa553d737ab852937df73b5ef339077870
执笔 <805073259@qq.com>
2019-04-22 13:19:18 +0800

    :sparkles: 社团更新

--- src/main/java/edu/blog/controller/admin/AdminLeagueController.java
+++ src/main/java/edu/blog/controller/admin/AdminLeagueController.java
@@ -4,7 +4,10 @@ import com.github.pagehelper.PageInfo;
 import edu.blog.core.base.controller.BaseAdminController;
 import edu.blog.core.mybatis.condition.MybatisCondition;
 import edu.blog.domain.League;
+import edu.blog.domain.LeagueUser;
+import edu.blog.dto.LeagueUserDTO;
 import edu.blog.mapper.LeagueMapper;
+import edu.blog.mapper.LeagueUserMapper;
 import edu.blog.service.LeagueService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * @author 执笔
@@ -24,9 +28,11 @@ import java.time.LocalDateTime;
 public class AdminLeagueController extends BaseAdminController {
 
     @Autowired
-    private LeagueService leagueService;
+    private LeagueService    leagueService;
     @Autowired
-    private LeagueMapper  leagueMapper;
+    private LeagueMapper     leagueMapper;
+    @Autowired
+    private LeagueUserMapper leagueUserMapper;
 
     /**
      * 社团列表
@@ -56,6 +62,10 @@ public class AdminLeagueController extends BaseAdminController {
     public String detail(@PathVariable Integer id, Model model) {
         League league = leagueMapper.selectByPrimaryKey(id);
         model.addAttribute(league);
+        // 成员信息
+        List<LeagueUserDTO> leagueUserList = leagueUserMapper.selectDto(new MybatisCondition()
+                .eq("lu.league_Id", league.getId()));
+        model.addAttribute("leagueUserList", leagueUserList);
         return "admin/league/update";
     }
 
@@ -86,6 +96,22 @@ public class AdminLeagueController extends BaseAdminController {
     public String add(Model model) {
         model.addAttribute(new League());
         return "admin/league/update";
+    }
+
+    /**
+     * 更新用户类型
+     *
+     * @param id
+     * @param type
+     * @return
+     */
+    @GetMapping("updateType/{id}/{type}")
+    public String updateType(@PathVariable Integer id, @PathVariable LeagueUser.LeagueUserTypeEnum type) {
+        LeagueUser leagueUser = leagueUserMapper.selectByPrimaryKey(id);
+        leagueUser.setType(type);
+        leagueUser.setUpdateTime(LocalDateTime.now());
+        leagueUserMapper.updateByPrimaryKeySelective(leagueUser);
+        return refresh();
     }
 
 }
